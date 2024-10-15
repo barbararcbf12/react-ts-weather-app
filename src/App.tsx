a89da0d0e37ce3c0d76d903dccd57892f3c9877c
Barbara Ferreira <bbf@charlietango.dk>
2024-10-15 13:00:09 +0200

    fix: use navigator.geolocation.getCurrentPosition callback correctly

--- src/App.tsx
+++ src/App.tsx
@@ -30,19 +30,17 @@ const initialViewState: ViewStateProps = {
   height: 100
 };
 
-const setMapCenter = (position: GeolocationPosition): Coordinates => {
-  if(position) return [position.coords.latitude, position.coords.longitude];
-  else return initialCoordinates;
+const setMapCenter = (position: Omit<GeolocationPosition, 'timestamp'>): Coordinates => {
+  return position ? [position.coords.latitude, position.coords.longitude] : initialCoordinates;
 };
-
-const position: any = navigator.geolocation.getCurrentPosition(setMapCenter);
 
 function App() {
   const initialTempUnit: string | null = window.localStorage.getItem('temperatureUnit');
   const [unit, setUnit] = useState<TemperatureUnitEnum>( initialTempUnit && JSON.parse(initialTempUnit) ? JSON.parse(initialTempUnit) : 'celsius');
   const [isFetching, setIsFetching] = useState<boolean>(false);
   const [viewState, setViewState] = useState<ViewStateProps>(initialViewState);
-  const [marker, setMarker] = useState<Coordinates>(setMapCenter(position));
+  const [position, setPosition] = useState<Coordinates>(initialCoordinates);
+  const [marker, setMarker] = useState<Coordinates>(setMapCenter({ coords: { latitude: position[0], longitude: position[1], accuracy: 0, altitude: null, altitudeAccuracy: null, heading: null, speed: null }}));
   const [weatherData, setWeatherData] = useState<WeatherData | undefined>(undefined);
 
   const getApiData = async (lat: number, long: number) => await fetchWeatherData(lat, long, setWeatherData, setIsFetching);
@@ -68,7 +66,11 @@ function App() {
   const handleOnMove = (viewState: ViewStateProps) => setViewState(viewState);
 
   useEffect(() => {
-    getApiData(setMapCenter(position)[0], setMapCenter(position)[1]);
+    navigator.geolocation.getCurrentPosition((pos) => {
+      const coords: Coordinates = setMapCenter(pos);
+      setPosition(coords);
+      getApiData(coords[0], coords[1]);
+    });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
