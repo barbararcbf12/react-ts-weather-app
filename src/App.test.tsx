7402df72009a7e1e9dc62c9a18cccd6458d82835
Barbara Ferreira <bbf@charlietango.dk>
2024-10-15 14:54:27 +0200

    fix: issues with tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,31 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-test("render <App> component without props", () => {
-  render(<App/>);
-  const title = screen.getByText(/App's name/i);
+jest.mock('./components/Map/Map', () => {
+  return () => <div>Mocked Map</div>;
+});
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
+beforeAll(() => {
+  (global.navigator as any).geolocation = {
+    getCurrentPosition: jest.fn((success) =>
+      success({
+        coords: {
+          latitude: 55.676098,
+          longitude: 12.568337,
+          accuracy: 100,
+        },
+      })
+    ),
+  };
+});
+
+test("renders <App> component without props", () => {
+  render(<App />);
+  const title = screen.getByText(/Weather app/i);
   expect(title).toBeInTheDocument();
 });
