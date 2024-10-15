33f9c13a798baa87a9572280429fe549e8e85581
Barbara Ferreira <bbf@charlietango.dk>
2024-10-15 17:22:52 +0200

    fix: fetch call to Open Weather api

--- src/api/fetchWeatherData.ts
+++ src/api/fetchWeatherData.ts
@@ -1,25 +1,13 @@
-import { WeatherData } from "../types";
-
 const WEATHER_AP_KEY = process.env.REACT_APP_WEATHER_API_URL;
 const OPEN_WEATHER_API_KEY = process.env.REACT_APP_OPEN_WEATHER_API_KEY;
 
 export async function fetchWeatherData(lat: number, lang: number) {
   try{
-    const res = await fetch(`${WEATHER_AP_KEY}?lat=${lat}&lon=${lang}&APPID=${OPEN_WEATHER_API_KEY}`,
-      // Adding mode as a second parameter to the fetch function solved the CORS issue
-      // but TS doesn't recognize the second parameter as a valid option,
-      // therefore I added @ts-ignore to ignore the error
-      { mode: 'cors' },
-      // @ts-ignore
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'
-        }
-      });
+    const res = await fetch(`${WEATHER_AP_KEY}?lat=${lat}&lon=${lang}&APPID=${OPEN_WEATHER_API_KEY}`);
+
     const data = await res.json();
 
-    const obj: WeatherData = {
+    return {
       city: data.name,
       country: data.sys.country,
       temperature: data.main.temp,
@@ -27,9 +15,7 @@ export async function fetchWeatherData(lat: number, lang: number) {
       icon: data.weather[0]?.icon ?? ''
     };
 
-    return obj;
-
   } catch (error) {
     console.error(error);
   }
-};
+}
