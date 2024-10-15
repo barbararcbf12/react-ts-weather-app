8371bf043a652c932a7fb130bf39569dc23e220c
Barbara Ferreira <bbf@charlietango.dk>
2024-10-15 16:45:37 +0200

    fix: remove and fix tests

--- src/__tests__/Card.test.tsx
+++ src/__tests__/Card.test.tsx
@@ -1,6 +1,7 @@
 import { render } from "@testing-library/react";
 import Card from "../components/Card";
+import { TemperatureUnitEnum } from "../types";
 
 test('should render <Card>', () => {
-  render(<Card isFetching={false} unit="celsius"/>);
+  render(<Card isFetching={false} unit={TemperatureUnitEnum.Celsius}/>);
 });
