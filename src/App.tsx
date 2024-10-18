5c277334bca85d1cf903c472ad78a6a0090a268c
Barbara Ferreira <bbf@charlietango.dk>
2024-10-18 09:20:30 +0200

    refactor: delete marker state and useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,26 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Header from "./components/Header";
 import Map from "./components/Map";
-import { fetchWeatherData } from "./api/fetchWeatherData";
 import Pin from "./components/Pin";
 import Card from "./components/Card";
 import { NavigationControl } from "react-map-gl";
-import { Coordinates, TemperatureUnitEnum, ViewStateProps, WeatherData } from "./types";
+import { Coordinates, TemperatureUnitEnum, ViewStateProps } from "./types";
 import { initialCoordinates, initialViewState } from "./store/initialData";
-
-const setMapCenter = (position: Omit<GeolocationPosition, 'timestamp'>): Coordinates => {
-  return position ? [position.coords.latitude, position.coords.longitude] : initialCoordinates;
-};
 
 function App() {
   const initialTempUnit: string | null = window.localStorage.getItem('temperatureUnit');
   const [unit, setUnit] = useState<TemperatureUnitEnum>( initialTempUnit && JSON.parse(initialTempUnit) ? JSON.parse(initialTempUnit) : 'celsius');
-  const [isFetching, setIsFetching] = useState<boolean>(false);
   const [viewState, setViewState] = useState<ViewStateProps>(initialViewState);
   const [position, setPosition] = useState<Coordinates>(initialCoordinates);
-  const [marker, setMarker] = useState<Coordinates>(setMapCenter({ coords: { latitude: position[0], longitude: position[1], accuracy: 0, altitude: null, altitudeAccuracy: null, heading: null, speed: null }}));
-  const [weatherData, setWeatherData] = useState<WeatherData | undefined>(undefined);
 
-  const handleOnClick = async (lat: number, long: number) => {
-    setMarker([lat, long]);
-    setViewState( (prevState) => ({
-        ...prevState,
-        longitude: long,
-        latitude: lat,
-        zoom: 8
-      }
-    ));
-    const data = await fetchWeatherData(lat, long);
-    setWeatherData(data);
+  const handleOnClick = (lat: number, long: number) => {
+    setPosition([lat, long]);
+    setViewState(prevState => ({
+      ...prevState,
+      longitude: long,
+      latitude: lat,
+      zoom: 8,
+    }));
   };
 
   const toggleUnit = () => {
@@ -45,21 +34,6 @@ function App() {
       setViewState(newViewState);
     }
   };
-
-  useEffect(() => {
-    // Set the user's current location as coordinates on the map
-    navigator.geolocation.getCurrentPosition(async (pos: GeolocationPosition) => {
-      const coords: Coordinates = setMapCenter(pos);
-      setPosition(coords);
-      setIsFetching(true);
-      const data = await fetchWeatherData(coords[0], coords[1]);
-      if(data) {
-        setWeatherData(data);
-        setIsFetching(false);
-      }
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
 
   return (
     <div className="relative h-screen">
@@ -75,8 +49,8 @@ function App() {
           onClick={handleOnClick}
           onMove={handleOnMove}
         >
-          <Pin marker={marker}>
-            <Card weather={ weatherData } isFetching={ isFetching } unit={ unit }/>
+          <Pin marker={position}>
+            <Card position={position} unit={unit}/>
           </Pin>
 
           <NavigationControl position="bottom-right" showCompass={true} />
